7c69314a0bc5c368deb31494f4f37660de52ce2c
Alexander Lohnau <alexander.lohnau@gmx.de>
2024-06-30 08:04:13 +0200

    Use respective kquitapp5/kquitepp6 CLI commands depending on KRunner version it is build for
    While it works as expected when it is around, we should better depend on an executable of the same major version that should for sure be around

--- install.sh
+++ install.sh
@@ -31,7 +31,7 @@ sudo make install
 # KRunner needs to be restarted for the changes to be applied
 if pgrep -x krunner > /dev/null
 then
-    kquitapp5 krunner
+    kquitapp$krunner_version krunner
 fi
 
 echo "Installation finished!";
--- uninstall.sh
+++ uninstall.sh
@@ -3,12 +3,13 @@
 # Exit if something fails
 set -e
 
+krunner_version=$(krunner --version | grep -oP "(?<=krunner )\d+")
 cd build
 sudo make uninstall
 
 # KRunner needs to be restarted for the changes to be applied
 if pgrep -x krunner > /dev/null
 then
-    kquitapp5 krunner
+    kquitapp$krunner_version krunner
 fi
 
